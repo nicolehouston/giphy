b2d12812af63d0df806fc60c303ded2e61a218af
Nicole Houston <nicole.houston@cox.net>
2018-05-21 09:17:09 -0700

    set up the ajax

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,29 @@
+// Array of topics
+var topics = ["iron man", "captain america", "thor", "wonder woman", "black panther", "superman", "black widow", "spiderman", "batman"];
+
+// Fuction that makes buttons for each topic
+function displayButtons(){
+    for (var i = 0; i < topics.length; i++){
+        var btn = $("<button>");
+        btn.attr("value", topics[i]);
+        btn.text(topics[i]);
+        $(".buttons").append(btn);
+    }
+}
+
+displayButtons();
+
+$(".buttons").on("click", "button", function(){
+
+    var superhero = $(this).val();
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + superhero + "&api_key=4RnlpSHJp6RDZ2NtQvbDsAsbbFtzQgjc";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        results = response.data;
+        
+    });
+})
